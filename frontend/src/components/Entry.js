dde2076cbacbb18d2ebee57286d794a7e8086377
Jonathan Xu <22625405+ViciousCupcake@users.noreply.github.com>
2021-05-26 17:39:25 -0700

    do some data validation (#27)

--- frontend/src/components/Entry.js
+++ frontend/src/components/Entry.js
@@ -5,24 +5,27 @@ import Likes from './Likes'
 function Entry(props) {
   var date = new Date(props.submission.submission_date);
   var altdescription = 'no image submitted';
-  if (props.submission.image){
+  if (props.submission.image) {
     altdescription = 'image was submitted'
   }
   // terrible date formatting hack from stack overflow lol
   date = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().split("T")[0];
   return (
     <div className="content-card">
-      <h1>{props.submission.title}</h1>
-      <h4>Post by {props.submission.first_name}</h4>
+      {props.submission.title && <h1>{props.submission.title}</h1>}
+      { props.submission.first_name ?
+        <h3>Post by {props.submission.first_name}</h3> :
+        <h3>Post by Anonymous</h3>
+      }
       <h5>{date}</h5>
       <p>{props.submission.content}</p>
-      {props.submission.image && 
-        <img src = {props.submission.image} alt = {altdescription} width = '200' height = '200'></img>}
+      {props.submission.image &&
+        <img src={props.submission.image} alt={altdescription} width='200' height='200'></img>}
       <p>
         <a href={`/post/${props.submission._id}`}> See more info</a>
       </p>
       <p>
-        <Likes likes = {props.submission.likes} id = {props.submission._id} />
+        <Likes likes={props.submission.likes} id={props.submission._id} />
       </p>
     </div>
   )
