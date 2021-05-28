55ceae66e9fc2624db40e1d3e40a7b369ffa361d
ViciousCupcake <22625405+ViciousCupcake@users.noreply.github.com>
2021-05-27 21:19:30 -0700

    fix dependencies

--- frontend/src/components/GoogleMaps.js
+++ frontend/src/components/GoogleMaps.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './styling/SinglePagePost.css';
 import '../App.css';
+import './styling/homepageButton.css'
+
 import MapContainer from './GoogleMapsLinker';
 
 
@@ -10,12 +12,12 @@ class GoogleMaps extends Component {
 
     render() {
         return (
-            <>
+            <div>
                 {/* TODO: button doesn't work for some reason, but does if you comment out mapcontainer */}
                 <button onClick={() => {window.location = '/'}} className="returnHomeButton">Return to Homepage</button>
                 <MapContainer/>
                 <p></p>
-            </>
+            </div>
         );
     }
 }
